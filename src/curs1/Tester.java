6aeda01509e36b2eebe64981bf61bfdd7bb03a56
Dragos Tanta <dragos.tanta@gmail.com>
2023-05-05 18:03:37 +0300

    curs1

---
+++ src/curs1/Tester.java
@@ -0,0 +1,43 @@
+package curs1;
+ // one line comment
+
+/*
+ * multi line comment
+ */
+
+//UpperCamelCase         //metoda_mea_care_face_lucruri(snake_case)
+//lowerCamelCase         //metodaMeaCareFaceLucruri
+
+public class Tester {     
+	//variabile
+	private String nume;
+	public int ratePerHour;
+	
+	
+	//getter
+	public String getNume() {
+		return nume;
+	}
+	//setter
+	public void setNume(String nume) {
+		this.nume = nume;
+	}
+	
+	
+	//Tester obj = new Tester();
+	/*
+	 * Tester --Numele clasei
+	 * obj - numele obiect
+	 * =  - operator de atriburie
+	 * new - cuvant cheie in java care ne ajuta sa instantiem o clasa
+	 * Tester() - constructor
+	 */
+	
+	//Tester obj = new Tester();
+	// obj.setNume("Oana");
+	
+	
+	
+	
+	
+}
